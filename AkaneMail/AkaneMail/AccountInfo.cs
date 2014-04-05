0f5dc1afd7436655110f22c7fdf012bf22c056d2
ゆうなんとか <yuunnxx@gmail.com>
2014-04-05 21:37:57 +0900

    リセットをこっちに移動

--- AkaneMail/AkaneMail/AccountInfo.cs
+++ AkaneMail/AkaneMail/AccountInfo.cs
@@ -37,5 +37,30 @@ namespace AkaneMail
         {
             get { return fromName + "<" + mailAddress + ">"; }
         }
+
+        /// <summary>
+        ///  アカウント情報(初期値)を設定します。
+        /// </summary>
+        public static void Reset()
+        {
+            AccountInfo.fromName = "";
+            AccountInfo.mailAddress = "";
+            AccountInfo.userName = "";
+            AccountInfo.passWord = "";
+            AccountInfo.smtpServer = "";
+            AccountInfo.popServer = "";
+            AccountInfo.popPortNumber = 110;
+            AccountInfo.smtpPortNumber = 25;
+            AccountInfo.apopFlag = false;
+            AccountInfo.deleteMail = false;
+            AccountInfo.popBeforeSMTP = false;
+            AccountInfo.popOverSSL = false;
+            AccountInfo.smtpAuth = false;
+            AccountInfo.autoMailFlag = false;
+            AccountInfo.getMailInterval = 10;
+            AccountInfo.popSoundName = "";
+            AccountInfo.bodyIEShow = false;
+            AccountInfo.minimizeTaskTray = false;
+        }
     }
 }
