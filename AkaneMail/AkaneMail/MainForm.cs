d2b5c1e399c19df0dea57c3e9fc144b34e81c40b
mog <dmog921@zoho.com>
2014-04-21 09:17:15 +0900

    フラグとメソッドの整理

--- AkaneMail/AkaneMail/MainForm.cs
+++ AkaneMail/AkaneMail/MainForm.cs
@@ -45,9 +45,6 @@ namespace AkaneMail
         // 添付付きメールの返信用文字列
         public string attachMailBody = "";
 
-        // 未読メールか既読メールかの確認フラグ
-        public bool checkNotYetReadMail = false;
-
         // nMailの致命的なエラーの確認フラグ(現状は64bitOSの32bit版DLL読み込みエラーのみ)
         public bool nMailError = false;
         #endregion
@@ -632,8 +629,6 @@ namespace AkaneMail
         /// <returns>デコード後のメッセージ</returns>
         private nMail.Attachment ExtractMessage(Mail mail)
         {
-            checkNotYetReadMail = mail.NotReadYet;
-
             Options.EnableDecodeBody();
 
             var attach = new nMail.Attachment();
@@ -661,9 +656,6 @@ namespace AkaneMail
             ClearAttachMenu();
 
             var attach = new nMail.Attachment();
-
-            // 未読メールの場合
-            checkNotYetReadMail = mail.NotReadYet;
 
             // 保存パスはプログラム直下に作成したtmpに設定する
             attach.Path = Application.StartupPath + @"\tmp";
@@ -1681,11 +1673,14 @@ namespace AkaneMail
         ///  選択されたメールを取得します。
         /// </summary>
         /// <param name="index">インデックス</param>
-        /// <param name="columnText">選択されているカラムの文字列</param>
         /// <returns></returns>
-        private Mail GetSelectedMail(object index, string columnText)
+        private Mail GetMailByIndex(object index)
         {
+            string columnText = listMail.Columns[0].Text;
+
             switch (columnText) {
+                case "名前":
+                    return null;
                 case "差出人":
                     return mailBox.Receive[(int)index];
                 case "宛先":
@@ -1915,15 +1910,10 @@ namespace AkaneMail
         private void listMail_DoubleClick(object sender, EventArgs e)
         {
             var item = listMail.SelectedItems[0];
-
-            // メールボックスのときは反応しない
-            if (listMail.Columns[0].Text == "名前") {
-                return;
+            var mail = GetMailByIndex(item.Tag);
+            if (mail != null) {
+                EditMail(mail, item);
             }
-
-           var mail = GetSelectedMail(item.Tag, listMail.Columns[0].Text);
-
-            EditMail(mail, item);
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
@@ -1998,6 +1988,7 @@ namespace AkaneMail
                 t.Join();
             }
             catch (MailLoadException) {
+                // 要修正！！：例外の発生源が別スレッド
                 errorFlag = true;
             }
 
@@ -2033,55 +2024,51 @@ namespace AkaneMail
 
         private void menuReplyMail_Click(object sender, EventArgs e)
         {
-            var item = listMail.SelectedItems[0];
-
             if (listMail.SelectedItems.Count == 0) {
                 return;
             }
 
+            var item = listMail.SelectedItems[0];
             // 表示機能はシンプルなものに変わる
-            var mail = GetSelectedMail(item.Tag, listMail.Columns[0].Text);
-
-            CreateReturnMail(mail);
+            var mail = GetMailByIndex(item.Tag);
+            if (mail != null) {
+                CreateReturnMail(mail);
+            }
         }
 
         private void listMail_Click(object sender, EventArgs e)
         {
             var item = listMail.SelectedItems[0];
-
-            if (listMail.Columns[0].Text == "名前") {
-                return;
+            var mail = GetMailByIndex(item.Tag);
+            if (mail != null) {
+                OpenMail(mail);
             }
-
-            var mail = GetSelectedMail(item.Tag, listMail.Columns[0].Text);
-
-            OpenMail(mail);
         }
 
         private void menuGetAttatch_Click(object sender, EventArgs e)
         {
-            ListViewItem item = listMail.SelectedItems[0];
-
             if (listMail.SelectedItems.Count == 0) {
                 return;
             }
 
             // 送信メール以外も展開できるように変更
-            var mail = GetSelectedMail(item.Tag, listMail.Columns[0].Text);
-
-            GetAttachMail(mail);
+            var mail = GetMailByIndex(listMail.SelectedItems[0].Tag);
+            if (mail != null) {
+                GetAttachMail(mail);
+            }
         }
 
         private void menuSaveMailFile_Click(object sender, EventArgs e)
         {
-            var item = listMail.SelectedItems[0];
-
             if (listMail.SelectedItems.Count == 0) {
                 return;
             }
 
             // どの項目でも保存できるように変更
-            var mail = GetSelectedMail(item.Tag, listMail.Columns[0].Text);
+            var mail = GetMailByIndex(listMail.SelectedItems[0].Tag);
+            if (mail == null) {
+                return;
+            }
 
             // ファイル名にメールの件名を入れる
             saveFileDialog1.FileName = mail.Subject;
@@ -2118,9 +2105,11 @@ namespace AkaneMail
 
         private void buttonAttachList_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            ListViewItem item = listMail.SelectedItems[0];
-
-            var mail = GetSelectedMail(item.Tag, listMail.Columns[0].Text);
+            var mail = GetMailByIndex(listMail.SelectedItems[0].Tag);
+            if (mail == null)
+            {
+                return;
+            }
 
             // ファイルを開くかの確認をする
             if (MessageBox.Show(e.ClickedItem.Text + "を開きますか？\nファイルによってはウイルスの可能性もあるため\n注意してファイルを開いてください。", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes) {
@@ -2262,15 +2251,14 @@ namespace AkaneMail
 
         private void menuFowerdMail_Click(object sender, EventArgs e)
         {
-            var item = listMail.SelectedItems[0];
-
             if (listMail.SelectedItems.Count == 0) {
                 return;
             }
 
-            var mail = GetSelectedMail(item.Tag, listMail.Columns[0].Text);
-
-            CreateFowerdMail(mail);
+            var mail = GetMailByIndex(listMail.SelectedItems[0].Tag);
+            if (mail != null) {
+                CreateFowerdMail(mail);
+            }
         }
         #endregion
     }
